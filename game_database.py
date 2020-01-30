6a3f97129ab3d1d8aecb6bebab11d35f837d8889
mbuckmaster03 <mbuckmaster@stcharlessd.org>
2020-01-30 12:29:55 -0600

    modified:   game_database.py 	modified:   game_lib.pickle 	modified:   reset_pickle_file.py

--- game_database.py
+++ game_database.py
@@ -6,20 +6,193 @@
 
 import pickle
 
+games = {}
+
+def info_search(item):
+    print()
+    print("Title: ", item[0])
+    print("Genre: ", item[1])
+    print("Developer: ", item[2])
+    print("Publisher: ", item[3])
+    print("Platform: ", item[4])
+    print("Release Date: ", item[5])
+    print("Rating: ", item[6])
+    print("Single/Multi: ", item[7])
+    print("Price: ", item[8])
+    print("Beat it?: ", item[9])    
+    print("Pruchase Date: ", item[10])
+    print("Notes: ", item[11])
+    print("----------------------")        
+
 def add_game():
-    print("\n  add_game function running")
+    new_key = len(games)+1
+    new_entry = []
+    valid = False
+    while not valid:
+        new_entry.append(input("What is the Title?"))
+        new_entry.append(input("What is the Genre?"))
+        new_entry.append(input("Who is the Developer?"))
+        new_entry.append(input("Who is the Publisher?"))
+        new_entry.append(input("What is the System?"))
+        new_entry.append(input("What is the Release Date?"))
+        new_entry.append(input("What is the Rating?"))
+        new_entry.append(input("Is it Singl/Multi/Both?"))
+        new_entry.append(input("What is the Price?"))
+        new_entry.append(input("Have you Beat it?"))
+        new_entry.append(input("What was the Purchase Date?"))
+        answer = input("Is this correct?")
+        if answer.lower() in ("yes", "y"):
+            valid = True
+        games[new_key] = entry
 def print_all():
-    print("\n  print_all function running")
-def search_by_name():
-    print("\n  search_by_name function running")
+    for key in games.keys():
+        item = games[key]
+        info_search(item)
+        
+def search_title():
+    lookup = input("\n What is the Title? ")
+    for key in games.keys():
+        if lookup in games[key][0]:
+            item = games[key]
+            info_search(item)
+def search_genre():
+    lookup = input("\n What is the Genre? ")
+    for key in games.keys():
+        if lookup in games[key][1]:
+            item = games[key]
+            info_search(item)
+            
+def search_developer():
+    lookup = input("What is the Developer? ")
+    for key in games.keys():
+        if lookup in games[key][3]:
+            item = games[key]
+            info_search(item)
+            
+def search_publisher():
+    lookup = input("What is the Publisher? ")
+    for key in games.keys():
+        if lookup in games[key][4]:
+            item = games[key]
+            info_search(item)
+            
+def search_system():
+    lookup = input("What is the System? ")
+    for key in games.keys():
+        if lookup in games[key][5]:
+            item = games[key]
+            info_search(item)
+            
+def search_release():
+    lookup = input("What is the Release Date? ")
+    for key in games.keys():
+        if lookup in games[key][6]:
+            item = games[key]
+            info_search(item)
+            
+def search_rating():
+    lookup = input("What is the Rating? ")
+    for key in games.keys():
+        if lookup in games[key][7]:
+            item = games[key]
+            info_search(item)
+            
+def search_single_multi():
+    lookup = input("Is the game Singleplayer, Multiplayer, or Both? ")
+    for key in games.keys():
+        if lookup in games[key][8]:
+            item = games[key]
+            info_search(item)
+            
+def search_price():
+    lookup = input("What is the Price? ")
+    for key in games.keys():
+        if lookup in games[key][9]:
+            item = games[key]
+            info_search(item)
+            
+def search_beat():
+    lookup = input("Have you Beaten It? ")
+    for key in games.keys():
+        if lookup in games[key][10]:
+            item = games[key]
+            info_search(item)
+            
+def search_purchase():
+    lookup = input("What is your Purchase Date? ")
+    for key in games.keys():
+        if lookup in games[key][11]:
+            item = games[key]
+            info_search(item)
+
+def search_by_item():
+    print('''
+    Search Specific
+    ---------------------------
+    
+    Search Menu
+    1)Title:
+    2)Genre:
+    3)Developer:
+    4)Publisher:
+    5)Platform:
+    6)Year Released:
+    7)Rating:
+    8)Single Or Multiplayer
+    9)Price:
+    10)Beat Game:
+    11)Purchase Date:
+    
+    Q)Quit
+    
+    ---------------------------
+    ''')
+    choice = input("What would you like to search for?" )
+    if choice == "1":
+        search_title()
+    elif choice == "2":
+        search_genre()
+    elif choice == "3":
+        search_developer()
+    elif choice == "4":
+        search_publisher()
+    elif choice == "5":
+        search_system()
+    elif choice == "6":
+        search_release()        
+    elif choice == "7":
+        search_rating()
+    elif choice == "8":
+        search_single_multi
+    elif choice == "9":
+        search_price()    
+    elif choice == "10":
+        search_beat()
+    elif choice == "11":
+        search_purchase()    
+    elif choice == "Q":
+        pass       
+    else:
+        print("*** NOT A VALID CHOICE ***\n")       
+    
 def remove_game():
     print("\n  remove_game function running")
+    
 def save_database():
-    print("\n  save_database function running")
+    data_file = open("game_lib.pickle", "wb")
+    pickle.dump(games, data_file)
+    data_file.close()
+    print("\n Data Saved!")
+    
 def quit():
-    print("\n  quit function running")
+    print("\n Goodbye.")
+    exit()
 
 keep_going = True
+
+data_file = open("game_lib.pickle", "rb")
+games = pickle.load(data_file)
+data_file.close()
 
 while keep_going:
     print("""
@@ -44,7 +217,7 @@ while keep_going:
     elif choice == "2":
         print_all()
     elif choice == "3":
-        search_by_name()
+        search_by_item()
     elif choice == "4":
         remove_game()
     elif choice == "5":
@@ -53,3 +226,5 @@ while keep_going:
         quit()    
     else:
         print("*** NOT A VALID CHOICE ***\n")    
+        
+print("\n Goodbye.")
--- reset_pickle_file.py
+++ reset_pickle_file.py
@@ -5,9 +5,11 @@
 """Create a default pickle file/reset pickle file"""
 
 import pickle
-
-games = {1 :['Halo 3','FPS','Bungee','Microsoft','Xbox 360','2007','10','both','$30.00','Yes','1/15/2008','This game blows chunks']}
+#Title, Genre, Developer, Publisher, System, Realse Date, Rating, Single/Multi/Both, Price, Purchase Date, Notes
+games = {1 :['Halo 3','FPS','Bungee','Microsoft','Xbox 360','2007','4','both','$30.00','Yes','1/15/2008','This game blows chunks'], 
+         2 :['The Legend of Zelda: Ocarina of Time','Action-Adventure','Nintendo','Nintendo','Nintendo 64','1998','10','single','$50.00','Yes','11/21/2011','This game is a masterpiece'],
+         3 :['The Legend of Zelda','FPS','Nintendo','Microsoft','Xbox 360','2007','10','both','$30.00','Yes','1/15/2008','This game blows chunks']}
 
 data_file = open("game_lib.pickle", "wb")
 pickle.dump(games, data_file)
-data_file.close
+data_file.close()
